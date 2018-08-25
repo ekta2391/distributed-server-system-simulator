24a99c3199626f1dcac407a82007c3a3290f7fed
Ekta Chavan <chavan.e@husky.neu.edu>
2018-08-25 03:01:47 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <SideNav />
+  </div>
+</template>
+
+<script>
+import SideNav from './components/SideNav.vue'
+export default {
+  name: 'app',
+  components: {
+    SideNav
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  color: #2c3e50;
+  background-color: black;
+}
+
+</style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="side-nav-container">
+    <div class="add-remove">
+      <span role="button" @click="addServer" class="add">+</span>
+      <span role="button" @click="removeServer" class="remove">-</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      serverCount: 4
+    }
+  },
+  methods: {
+    addServer () {
+      this.serverCount++
+      console.log(this.serverCount)
+    },
+    removeServer () {
+      this.serverCount--
+      console.log(this.serverCount)
+    }
+  },
+  computed: {
+    servers () {
+      let servers = this.serverCount
+      return servers
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+.side-nav-container {
+  width: 20%;
+  height: 100%;
+  background-color: #4F4E4E;
+}
+
+.add-remove {
+  margin-top: 40px;
+  margin: auto;
+  width: 80%;
+  height: 80px;
+  color: white;
+  font-size: 22px;
+  font-weight: bold;
+}
+.add, .remove {
+  display: inline-block;
+  padding: 20px;
+  margin-left: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
